1aa3442cf20f183929ab5600bb2f8434a3e55d19
José Murillo <jose.nunes@pede.ai>
2022-04-07 10:44:32 -0300

    adjust: header height in percentage to show image

--- src/screens/CharacterDetails/styles.ts
+++ src/screens/CharacterDetails/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components/native';
 import Icon from 'react-native-vector-icons/AntDesign';
-import {RFValue} from 'react-native-responsive-fontsize';
 import {StatusProps} from './types';
+import {ScrollView} from 'react-native';
 
 export const Container = styled.View`
   flex: 1;
@@ -10,7 +10,7 @@ export const Container = styled.View`
 export const Header = styled.View`
   position: relative;
   width: 100%;
-  height: ${RFValue(437, 900)}px;
+  height: 55%;
 `;
 
 export const ImageCharacter = styled.View`
@@ -41,8 +41,8 @@ export const ArrowBack = styled(Icon)``;
 
 export const Heart = styled(Icon)``;
 
-export const ContentInfo = styled.View`
-  padding: 31px 20px 39px 20px;
+export const ContentInfo = styled(ScrollView)`
+  padding: 31px 20px 0px 20px;
 `;
 
 export const HeaderSection = styled.View`
@@ -60,15 +60,14 @@ export const NameCharacter = styled.Text`
 `;
 
 export const AboutSection = styled.View`
-  margin-top: 30px;
-  width: 245px;
+  margin-top: 15px;
 
   flex-direction: row;
   justify-content: space-between;
 `;
 
 export const LocationSection = styled.View`
-  margin-top: 30px;
+  margin-top: 15px;
 `;
 
 export const Info = styled.Text`
@@ -95,12 +94,14 @@ export const Title = styled.Text`
   color: ${props => props.theme.colors.text.dark};
 `;
 
-export const WrapperInfo = styled.View``;
+export const WrapperInfo = styled.View`
+  width: 50%;
+`;
+export const Footer = styled.View`
+  width: 100%;
+`;
 
 export const ButtonSearchGoogle = styled.TouchableOpacity`
-  position: absolute;
-  bottom: 0;
-
   width: 100%;
   height: 56px;
   justify-content: center;
