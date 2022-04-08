8ae1ce4a725ba0bba2b727beb3b7ceb08f5331ee
José Murillo <jose.nunes@pede.ai>
2022-04-08 12:11:12 -0300

    test: updated when no have none characters

--- src/screens/Home/home.spec.tsx
+++ src/screens/Home/home.spec.tsx
@@ -5,19 +5,23 @@ import {Home} from '../../screens/Home';
 import {Providers} from '../../utils/tests/helper';
 import {server} from './mock';
 
-// jest.mock('../../hooks/useCharacters', () => ({
-//   ...jest.requireActual('../../hooks/useCharacters'),
-// }));
-
 describe('<Home />', () => {
   beforeAll(() => server.listen());
   afterEach(() => server.resetHandlers());
   afterAll(() => server.close());
+
   it('should have show correctly counter characters', () => {
     const {getByTestId} = render(<Home />, {wrapper: Providers});
     const textCounterListing = getByTestId('count-listing').children[0];
 
-    expect(textCounterListing).toEqual('0 personagem');
+    expect(textCounterListing).toEqual('Nenhum personagem');
+  });
+
+  it('render name mocked', async () => {
+    const {findByText} = render(<Home />, {wrapper: Providers});
+    const RickSanchezTwoText = await findByText('Rick Sanchez 2');
+
+    expect(RickSanchezTwoText).toBeTruthy();
   });
 
   it('should must have render characters counter after load data of API', async () => {
